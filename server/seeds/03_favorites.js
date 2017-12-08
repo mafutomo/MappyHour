b2776e8c3fe15294ca2d34ea2cc18f7cfc661322
Brent Schroder <schroder.brent@gmail.com>
2017-12-08 15:07:55 -0700

    favorites seed + migrations

---
+++ server/seeds/03_favorites.js
@@ -0,0 +1,17 @@
+
+exports.seed = function(knex, Promise) {
+      return knex('favorites').insert([
+        {user_id: 1, restaurant_id: 2},
+        {user_id: 2, restaurant_id: 1},
+        {user_id: 2, restaurant_id: 3},
+        {user_id: 3, restaurant_id: 1},
+        {user_id: 3, restaurant_id: 2},
+        {user_id: 2, restaurant_id: 4},
+        {user_id: 4, restaurant_id: 1},
+        {user_id: 4, restaurant_id: 2},
+        {user_id: 4, restaurant_id: 5},
+        {user_id: 4, restaurant_id: 3}
+      ]).then(function(){
+        return knex.raw("SELECT setval('favorites_id_seq', (SELECT MAX(id) FROM favorites))");
+    });
+};
