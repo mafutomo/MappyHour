59284cd1e428af91c6b3325ac32382eab7e85f08
Brent Schroder <schroder.brent@gmail.com>
2017-12-08 14:21:58 -0700

    restaurants migrations

---
+++ server/migrations/20171208134543_restraunts.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('restaurants', table => {
+    table.increments()
+    table.string('name').notNullable()
+    table.string('address').notNullable()
+    table.string('phonenumber').notNullable()
+    table.integer('latitude').notNullable()
+    table.integer('longitude').notNullable()
+    table.text('description').notNullable().defaultsTo('description unavialable')
+    table.text('url')
+    table.text('picture').notNullable().defaultsTo('http://www.broadviewent.com/data/uploads/image-unavailable-icon.png')
+    table.text('happyHourMenu').notNullable().defaultsTo('menu info unavialble')
+    table.integer('averageRating').notNullable().defaultsTo(3)
+    table.timestamps(true, true)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('restaurants')
+};
