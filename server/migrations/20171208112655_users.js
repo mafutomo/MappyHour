b2ef031654920bb6c1c69641ae8d22fa4190f51f
Brent Schroder <schroder.brent@gmail.com>
2017-12-08 12:14:43 -0700

    migration and seeds for users

--- server/migrations/20171208112655_users.js
+++ server/migrations/20171208112655_users.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', table => {
+    table.increments()
+    table.string('firstName').notNullable()
+    table.string('lastName').notNullable()
+    table.string('email').notNullable().unique()
+    table.binary('password',60).notNullable().unique()
+    table.timestamps(true, true)
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users')
 };
