32c0d736dd71d23aa576e2a6365cbddfb139ed55
Robert Quan <rob.quan@gmail.com>
2017-12-10 11:14:12 -0700

    synching with group

---
+++ server/server.js
@@ -0,0 +1,25 @@
+'use strict';
+const express = require('express')
+const app = express()
+const port = 3000
+const router = require('./routes/routes.js')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+// body parser
+// cookie parser at some point
+app.use(express.static('../public'))
+
+app.use('/', router)
+
+app.use((req,res,sendit)=>{
+  res.sendStatus(506)
+})
+
+
+app.listen(port, ()=>{
+  console.log(`you are now tunned to ${port} "the bees knees"`)
+})
+
+
+module.exports = app;
