cf7b18275fe6bfa3446a2945f2f9e6ba26b8da33
Robert Quan <rob.quan@gmail.com>
2017-12-12 16:23:47 -0700

    testing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require('path')
 
 // app.use(express.static('/public'))
 
-app.use(express.static('/public/index.html')
+app.use(express.static('/public/index.html'))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
