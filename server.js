ea83d8085b2d4b9c89271c890dde755b313d24ee
Robert Quan <rob.quan@gmail.com>
2017-12-12 15:58:30 -0700

    made changes to server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const path = require('path')
 // body parser
 // cookie parser at some point
 
-app.use(express.static(path.join('public')))
+// app.use(express.static('/',path.join('public')))
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
