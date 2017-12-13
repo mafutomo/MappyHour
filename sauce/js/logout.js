13be369e9942fe44c8615ed9963fc855fcd6efa8
Robert Quan <rob.quan@gmail.com>
2017-12-13 15:22:10 -0700

    Added toast to logout.js

--- sauce/js/logout.js
+++ sauce/js/logout.js
@@ -1,8 +1,11 @@
 $( document ).ready(function(){
-$('.logout').click(function(evenet){
+$('.logout').click(function(event){
   console.log('clicked');
   localStorage.clear();
 })
-
-
+$('.mapButton').click(function(){
+  var restaurant = $(event.target).closest('.content').find('.firstHeading').text()
+  console.log(restaurant);
+   Materialize.toast( `${restaurant} saved to favorites` , 4000)
+})
 })
